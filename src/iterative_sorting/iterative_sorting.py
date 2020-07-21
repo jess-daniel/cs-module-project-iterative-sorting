fcf14bdf1af7e30db2e2f3829ed9e0f44c0415ba
Jess Daniel <jessdaniel474@gmail.com>
2020-07-21 11:21:13 -0400

    sorting and searching done

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,16 +1,15 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
-
-
-        # TO-DO: swap
-        # Your code here
+    for i in range(len(arr)-1):
+        smallest_index = i
+        # j represents remaining elements
+        for j in range(i+1, len(arr)):
+            # Update the min_index if the element at j is lower than it
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+        # After finding the lowest item of the unsorted regions, swap with the first unsorted item
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
     return arr
 
@@ -18,8 +17,17 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
-
+    n = len(arr) 
+    # Traverse through all array elements 
+    for i in range(n-1):   
+        # Last i elements are already in place 
+        for j in range(0, n-i-1): 
+  
+            # traverse the array from 0 to n-i-1 
+            # Swap if the element found is greater 
+            # than the next element 
+            if arr[j] > arr[j+1] : 
+                arr[j], arr[j+1] = arr[j+1], arr[j] 
     return arr
 
 '''
