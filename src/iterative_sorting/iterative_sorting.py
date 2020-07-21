31026126034c3735eaaf97d4c8376b0e91682868
Jess Daniel <jessdaniel474@gmail.com>
2020-07-21 15:48:51 -0400

    adds comments

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -18,15 +18,13 @@ def selection_sort(arr):
 def bubble_sort(arr):
     # Your code here
     n = len(arr) 
-    # Traverse through all array elements 
+    # loop through array
     for i in range(n-1):   
         # Last i elements are already in place 
         for j in range(0, n-i-1): 
-  
-            # traverse the array from 0 to n-i-1 
+            # loop through the array from 0 to n-i-1 
             # Swap if the element found is greater 
-            # than the next element 
-            if arr[j] > arr[j+1] : 
+            if arr[j] > arr[j+1]: 
                 arr[j], arr[j+1] = arr[j+1], arr[j] 
     return arr
 
